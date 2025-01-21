80ef35ead2960b02b215666945f348af7563dbaa
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-01-21 11:49:55 +0530

    Create post cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,48 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <div className="image">
+        <img src="https://images.pexels.com/photos/29506646/pexels-photo-29506646/free-photo-of-cozy-minimalist-holiday-living-room-scene.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="entry image not found" />
+        </div>
+        <div className="texts">
+          <h2>Personal blog of my LIFE</h2>
+          <p>This is blog describe the thought of a person who is Author of this blog here you might get information about specific topic</p>
+        </div>
+        
+      </div>
+
+      <div className="post">
+      <div className="image">
+        <img src="https://images.pexels.com/photos/29506646/pexels-photo-29506646/free-photo-of-cozy-minimalist-holiday-living-room-scene.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="entry image not found" />
+        </div>
+        <div className="texts">
+          <h2>Personal blog of my LIFE</h2>
+          <p>This is blog describe the thought of a person who is Author of this blog here you might get information about specific topic</p>
+        </div>
+        
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://images.pexels.com/photos/29506646/pexels-photo-29506646/free-photo-of-cozy-minimalist-holiday-living-room-scene.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="entry image not found" />
+        </div>
+        <div className="texts">
+          <h2>Personal blog of my LIFE</h2>
+          <p>This is blog describe the thought of a person who is Author of this blog here you might get information about specific topic</p>
+        </div>
+        
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://images.pexels.com/photos/29506646/pexels-photo-29506646/free-photo-of-cozy-minimalist-holiday-living-room-scene.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="entry image not found" />
+        </div>
+        <div className="texts">
+          <h2>Personal blog of my LIFE</h2>
+          <p>This is blog describe the thought of a person who is Author of this blog here you might get information about specific topic</p>
+        </div>
+        
+      </div>
     </main>
-    
   )
 }
 
